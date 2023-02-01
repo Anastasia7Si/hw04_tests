748850b8846f9d9aa66a351ef9d16b7607028c29
Анастасия Пушкарная <pushckarnaya.anastasia@yandex.ru>
2023-02-01 12:15:55 +0300

    Отправка на 1ое ревью

---
+++ yatube/users/tests/test_views.py
@@ -0,0 +1,39 @@
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from django import forms
+
+User = get_user_model()
+
+
+class SignUpViewsTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='testUser')
+        cls.user_client = Client()
+        cls.user_client.force_login(cls.user)
+
+        cls.SIGNUP = reverse('users:signup')
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def test_about_page_uses_correct_template(self):
+        response = self.user_client.get(SignUpViewsTests.SIGNUP)
+        self.assertTemplateUsed(response, 'users/signup.html')
+
+    def test_signup_page_show_correct_context(self):
+        response = self.user_client.get(SignUpViewsTests.SIGNUP)
+        form_fields = {
+            'first_name': forms.fields.CharField,
+            'last_name': forms.fields.CharField,
+            'username': forms.fields.CharField,
+        }
+
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context.get('form').fields.get(value)
+                self.assertIsInstance(form_field, expected)
